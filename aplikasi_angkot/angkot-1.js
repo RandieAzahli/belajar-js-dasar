ae68785fd52de7753e650cb8cc31ce749fb0a7aa
RandieAzahli <randyboled@outlook.com>
2022-06-15 22:40:31 +0700

    aplikasi angkot sederhana

---
+++ aplikasi_angkot/angkot-1.js
@@ -0,0 +1,8 @@
+// * angkot-1
+
+const jumlahAngkot = 10;
+let noAngkot = 1;
+while (noAngkot <= jumlahAngkot) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+    noAngkot++;
+}
