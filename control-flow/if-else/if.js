7472ff9e7dfcc352d738f57849d7fd96834b7581
RandieAzahli <randyboled@outlook.com>
2022-06-18 13:49:46 +0700

    modify file if.js

--- control-flow/if-else/if.js
+++ control-flow/if-else/if.js
@@ -3,9 +3,9 @@
 const angka = prompt('Masukkan Angka : ');
 
 if (angka % 2 === 0) {
-    console.log('angka ' + 'adalah angka genap');
+    console.log(angka + ' adalah angka genap');
 } else if (angka % 2 === 1) {
-    console.log('angka ' + 'adalah angka ganjil');
+    console.log(angka + ' adalah angka ganjil');
 } else {
-    console.log('Yang anda masukkan bukan berupa angka');
+    alert('Yang anda masukkan bukan berupa angka');
 }
