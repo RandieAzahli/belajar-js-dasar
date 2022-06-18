1f998f01560edcb24e9ec3807b677b2da0463a0c
RandieAzahli <randyboled@outlook.com>
2022-06-19 01:20:44 +0700

    modify suit game

--- suit_game/script.js
+++ suit_game/script.js
@@ -1,40 +1,48 @@
 // * Suit Game
 
-// ? menangkap pilihan player
-const player = prompt('Pilih : gajah, semut, orang ?');
+let tanya = true;
 
-// ? menangkap bilangan computer
-// ? menangkap bilangan random
-let computer = Math.random();
+while (tanya) {
+    // ? menangkap pilihan player
+    const player = prompt('Pilih : gajah, semut, orang ?');
 
-if (computer < 0.34) {
-    computer = 'gajah';
-} else if (computer >= 0.34 && computer <= 0.67) {
-    computer = 'orang';
-} else {
-    computer = 'semut';
-}
+    // ? menangkap bilangan computer
+    // ? menangkap bilangan random
+    let computer = Math.random();
 
-// ? menentukan rules
-let result = '';
-if (computer == player) {
-    result = 'SERI !';
-} else if (computer == 'gajah') {
-    if (player == 'orang') {
-        result = 'KALAH !';
+    if (computer < 0.34) {
+        computer = 'gajah';
+    } else if (computer >= 0.34 && computer <= 0.67) {
+        computer = 'orang';
     } else {
-        result = 'MENANG !';
+        computer = 'semut';
     }
-} else if (computer == 'semut') {
-    result = (player == 'gajah') ? 'KALAH !' : 'MENANG !';
-} else if (computer == 'orang') {
-    result = (player == 'semut') ? 'KALAH !' : 'MENANG !';
-} 
-else {
-    result = 'Anda memasukkan pilihan yang salah !';
+
+    // ? menentukan rules
+    let result = '';
+    if (player == computer) {
+        result = 'SERI !';
+    } else if (player == 'gajah') {
+        if (computer == 'orang') {
+            result = 'MENANG !';
+        } else {
+            result = 'KALAH !';
+        }
+    } else if (player == 'semut') {
+        result = computer == 'gajah' ? 'MENANG !' : 'KALAH !';
+    } else if (player == 'orang') {
+        result = computer == 'semut' ? 'MENANG !' : 'KALAH !';
+    } else {
+        result = 'Anda memasukkan pilihan yang salah !';
+    }
+
+    // ? tampilkan hasilnya
+    console.log('Computer : ' + computer);
+    console.log('Player : ' + player);
+    console.log('Hasil : ' + result);
+    console.log('\n');
+
+    tanya = confirm('Coba lagi ?');
 }
 
-// ? tampilkan hasilnya
-console.log('Computer : ' + computer);
-console.log('Player : ' + player);
-console.log('Hasil : ' + result);
+alert('Terima kasih sudah bermain ...');
