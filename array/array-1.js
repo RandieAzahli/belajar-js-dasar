d479b88409bd77efcf1c470cdd28e43c6a8164a3
RandieAzahli <randyboled@outlook.com>
2022-06-19 20:18:40 +0700

    membuat file array-1.js

---
+++ array/array-1.js
@@ -0,0 +1,9 @@
+// * array
+
+const hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at", 'Sabtu', 'Minggu'];
+
+for (let i = 0; i < hari.length; ++i) {
+    console.log(hari[i]);
+}
+
+console.log(hari);
