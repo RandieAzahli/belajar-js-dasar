fdd2d83ec7ab1484690c4668501c03199e342cdd
RandieAzahli <randyboled@outlook.com>
2022-06-27 23:17:54 +0700

    update file

--- array/array-1.js
+++ array/array-1.js
@@ -1,9 +1,9 @@
-// * array
+// * array loop
 
-const hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at", 'Sabtu', 'Minggu'];
+const hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"];
 
 for (let i = 0; i < hari.length; ++i) {
-    console.log(hari[i]);
+  console.log(hari[i]);
 }
 
 console.log(hari);
