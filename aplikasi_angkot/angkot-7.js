8b59801c420397013a60951a57bb4e72a2d51286
RandieAzahli <randyboled@outlook.com>
2022-07-05 20:00:53 +0700

    membuat file object-1.js

--- aplikasi_angkot/angkot-7.js
+++ aplikasi_angkot/angkot-7.js
@@ -3,31 +3,31 @@ const penumpang = [];
 
 // * Tambah penumpang
 const tambahPenumpang = function (namaPenumpang, penumpang) {
-	// * jika angkot kosong
-	if (penumpang.length == 0) {
-		penumpang.push(namaPenumpang);
-		return penumpang;
-	} else {
-		// * telusuri seluruh kursi
-		for (let i = 0; i < penumpang.length; ++i) {
-			if (penumpang[i] == undefined) {
-				penumpang[i] = namaPenumpang;
+    // * jika angkot kosong
+    if (penumpang.length == 0) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+    } else {
+        // * telusuri seluruh kursi
+        for (let i = 0; i < penumpang.length; ++i) {
+            if (penumpang[i] == undefined) {
+                penumpang[i] = namaPenumpang;
 
-				return penumpang;
-			}
-			// * jika ada nama yang sama
-			else if (penumpang[i] == namaPenumpang) {
-				console.log("Penumpang dengan nama " + namaPenumpang + " sudah ada di dalam angkot ...");
-				return penumpang;
-			}
-			// * jika kursi sudah terisi semua
-			else if (i == penumpang.length - 1) {
-				penumpang.push(namaPenumpang);
-				return penumpang;
-			}
-		}
-	}
-	return penumpang;
+                return penumpang;
+            }
+            // * jika ada nama yang sama
+            else if (penumpang[i] == namaPenumpang) {
+                console.log("Penumpang dengan nama " + namaPenumpang + " sudah ada di dalam angkot ...");
+                return penumpang;
+            }
+            // * jika kursi sudah terisi semua
+            else if (i == penumpang.length - 1) {
+                penumpang.push(namaPenumpang);
+                return penumpang;
+            }
+        }
+    }
+    return penumpang;
 };
 
 tambahPenumpang("randi", penumpang);
@@ -40,20 +40,20 @@ tambahPenumpang("sandi", penumpang);
 console.log(penumpang);
 
 const turunPenumpang = function (namaPenumpang, penumpang) {
-	if (penumpang.length == 0) {
-		console.log("Angkot ini kosong...");
-	} else {
-		for (let i = 0; i < penumpang.length; ++i) {
-			if (namaPenumpang == penumpang[i]) {
-				penumpang[i] = undefined;
-				console.log("Penumpang " + namaPenumpang + " telah turun ...");
-				return penumpang;
-			} else if (i == penumpang.length - 1 && penumpang[i] != namaPenumpang) {
-				console.log("Penumpang " + namaPenumpang + " enggak ada namanya ...");
-				return penumpang;
-			}
-		}
-	}
+    if (penumpang.length == 0) {
+        console.log("Angkot ini kosong...");
+    } else {
+        for (let i = 0; i < penumpang.length; ++i) {
+            if (namaPenumpang == penumpang[i]) {
+                penumpang[i] = undefined;
+                console.log("Penumpang " + namaPenumpang + " telah turun ...");
+                return penumpang;
+            } else if (i == penumpang.length - 1 && penumpang[i] != namaPenumpang) {
+                console.log("Penumpang " + namaPenumpang + " enggak ada namanya ...");
+                return penumpang;
+            }
+        }
+    }
 };
 
 turunPenumpang("sandi", penumpang);
---
+++ object/object-1.js
@@ -0,0 +1,13 @@
+// * membuat object
+
+const mahasiswa = {
+    nama: "Randie",
+    umur: 18,
+    ips: [3.0, 2.5, 3.2],
+    alamat: {
+        jalan: "Jln.Cinambo Indah",
+        kota: "Bandung",
+        provinsi: "Jawa Barat",
+    },
+};
+console.log(mahasiswa);
