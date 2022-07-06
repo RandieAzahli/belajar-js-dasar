4ee8907336a938b534821c66d1ee5387f0fac239
RandieAzahli <randyboled@outlook.com>
2022-07-07 00:19:45 +0700

    manipulation DOM element

---
+++ DOM/manipulation/element.js
@@ -0,0 +1,27 @@
+// * Manipulation Element
+
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "<em>Randie Azahli</em>";
+
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "Hello World";
+
+// const judul = document.querySelector("#judul");
+// judul.style.color = "lightblue";
+// judul.style.backgroundColor = "salmon";
+
+// const judul = document.getElementsByTagName("h1")[0];
+// // judul.setAttribute("name", "randi");
+// const tempJdl = judul.getAttribute("id");
+// console.log(tempJdl);
+
+// const a = document.querySelector("section#a a");
+
+// a.setAttribute("id", "link");
+// const tmpA = a.getAttribute("href");
+// console.log(tmpA);
+
+// a.removeAttribute("href");
+
+// const p2 = document.querySelector(".p2");
+// doc.classList.method
