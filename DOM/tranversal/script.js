708ee7f4a093acefa658f0de9c1a3235a022fb62
RandieAzahli <randyboled@outlook.com>
2022-07-08 16:01:46 +0700

    dom tranversal

---
+++ DOM/tranversal/script.js
@@ -0,0 +1,28 @@
+// * events handling
+
+// // tangkap class close
+// const close = document.querySelector(".close");
+
+// // tangkap container card
+// const card = document.querySelector(".card");
+
+// // events handler
+// close.addEventListener("click", function () {
+// 	card.style.display = "none";
+// });
+
+// * DOM tranversal
+
+// // tangkap tombol close
+const close = document.querySelectorAll(".close");
+// for (let i = 0; i < close.length; ++i) {
+// 	close[i].addEventListener("click", function (e) {
+// 		e.target.parentElement.style.display = "none";
+// 	});
+// }
+
+close.forEach(function (el) {
+	el.addEventListener("click", function (e) {
+		e.target.parentElement.style.display = "none";
+	});
+});
